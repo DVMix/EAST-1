835a0679ea42640df7632d24f6b3baee83bcae2d
dvmixalkin <dvmixalkin@gmail.com>
2023-04-11 11:37:58 +0300

    add flag for printer

--- loss.py
+++ loss.py
@@ -27,7 +27,7 @@ class Loss(nn.Module):
 		super(Loss, self).__init__()
 		self.weight_angle = weight_angle
 
-	def forward(self, gt_score, pred_score, gt_geo, pred_geo, ignored_map):
+	def forward(self, gt_score, pred_score, gt_geo, pred_geo, ignored_map, print_logs=False):
 		if torch.sum(gt_score) < 1:
 			return torch.sum(pred_score + pred_geo) * 0
 		
@@ -37,5 +37,6 @@ class Loss(nn.Module):
 		angle_loss = torch.sum(angle_loss_map*gt_score) / torch.sum(gt_score)
 		iou_loss = torch.sum(iou_loss_map*gt_score) / torch.sum(gt_score)
 		geo_loss = self.weight_angle * angle_loss + iou_loss
-		print('classify loss is {:.8f}, angle loss is {:.8f}, iou loss is {:.8f}'.format(classify_loss, angle_loss, iou_loss))
+		if print_logs:
+			print('classify loss is {:.8f}, angle loss is {:.8f}, iou loss is {:.8f}'.format(classify_loss, angle_loss, iou_loss))
 		return geo_loss + classify_loss
